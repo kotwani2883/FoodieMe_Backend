e6e7d43bc54402f87eb29c9e294447eec9c273e6
unknown <palakkotwani2883@gmail.com>
2021-08-12 22:54:43 +0530

    MAde changes

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get("/", (req, res) => {
 app.use("/api/pins/",pinRoute);
 app.use("/api/users/",userRoute);
 
-app.listen(5000, () => {
+
+app.listen(process.env.PORT||5000, () => {
     console.log("Server is up and Running")
 })
